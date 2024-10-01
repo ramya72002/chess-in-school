70a6af8ea1e1158a39f9253d9259a9148ad8a12e
ramya72002 <nsriramya7@gmail.com>
2024-10-01 12:04:41 +0530

    pawn course added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import SignIn from "./signin/page";
-// import Sidebar from "./sidebar"
 
 export default function Home() {
   return (
     <main>
       <SignIn />
-      {/* <Sidebar /> */}
       
     
     
