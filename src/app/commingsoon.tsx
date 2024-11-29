c0c2771e7ec6fa5d7cf7d7c3d92b87e8f6730f89
ramya72002 <nsriramya7@gmail.com>
2024-11-29 14:01:09 +0530

    logo changed for small devices

--- src/app/commingsoon.tsx
+++ src/app/commingsoon.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useEffect } from 'react';
 import './commingsoon.scss'; // SCSS file for styling the modal
 
 interface ModalProps {
@@ -9,11 +9,19 @@ interface ModalProps {
 }
 
 const Commingsoon: React.FC<ModalProps> = ({ isOpen, onClose, content }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'; // Prevent scroll when modal is open
+    } else {
+      document.body.style.overflow = 'auto'; // Re-enable scroll when modal is closed
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
-    <div className="modalOverlay">
-      <div className="modalContent">
+    <div className="modalOverlay" onClick={onClose}>
+      <div className="modalContent" onClick={(e) => e.stopPropagation()}>
         <button className="closeButton" onClick={onClose}>X</button>
         <div className="modalBody">{content}</div>
       </div>
